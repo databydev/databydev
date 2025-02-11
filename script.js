571b48ed260e8f8ad1d9ca2c3d93dba1c6ba28c2
databydev <166037182+databydev@users.noreply.github.com>
2025-02-12 00:31:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var divElement = document.getElementById('viz1739290248904');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    vizElement.style.width = '100%';
-    vizElement.style.maxWidth = '900px';
-    vizElement.style.height = '600px';
+window.onload = function() {
+    var divElement = document.getElementById('vizContainer');
+    var vizElement = document.createElement("object");
+
+    vizElement.setAttribute("class", "tableauViz");
+    vizElement.setAttribute("style", "width:100%; height:85vh;");
+    vizElement.setAttribute("data-tabs", "no");
+
+    // Set up Tableau parameters
+    vizElement.innerHTML = `
+        <param name="host_url" value="https://public.tableau.com/">
+        <param name="embed_code_version" value="3">
+        <param name="site_root" value="">
+        <param name="name" value="ResidentialComplexStoryboard/Story1">
+        <param name="tabs" value="no">
+        <param name="toolbar" value="yes">
+        <param name="static_image" value="https://public.tableau.com/static/images/Re/ResidentialComplexStoryboard/Story1/1.png">
+        <param name="animate_transition" value="yes">
+        <param name="display_static_image" value="yes">
+        <param name="display_spinner" value="yes">
+        <param name="display_overlay" value="yes">
+        <param name="display_count" value="yes">
+        <param name="language" value="en-US">
+        <param name="filter" value="publish=yes">
+    `;
 
     var scriptElement = document.createElement('script');
     scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-});
+
+    divElement.appendChild(vizElement);
+    divElement.appendChild(scriptElement);
+};
